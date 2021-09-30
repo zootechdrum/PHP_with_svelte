7712dd38df4f7b96553f89d426a296446aec7eff
Cesar_Gomez <zootechdrum@gmail.com>
2021-09-30 11:35:23 -0700

    added cloud image delete unwanted pages implemnted router for page

---
+++ src/pages/Login.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+
+<div class="text-center container">
+    <div>
+        <img src="./img/fleetwood-logo.png" alt="fleetwood-logo">
+        <div>
+            <h1>SNAP CLOUD <span><img src="./img/cloud.png" alt="icon of a cloud"></span></h1>
+        </div>
+    </div>
+</div>
+
+<style>
+  .container {
+      height:100vh;
+      display:grid;
+      justify-content:center;
+  }
+
+</style>
